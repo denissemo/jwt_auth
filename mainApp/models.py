282fd92dbdac7d1e225c53c825a834ada26a9bfb
denissemo <denissemo1999@gmail.com>
2019-03-11 00:09:29 +0200

    add rest framework auth to urls

--- mainApp/models.py
+++ mainApp/models.py
@@ -8,3 +8,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
